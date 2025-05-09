53daf41fcddb2e8fb34aa305377306517cb97cc8
Brian Tomlinson <darthlukan@gmail.com>
2025-05-08 23:06:48 -0700

    Multi-change
    - Update package requirements
    - Set some text so we can see something in the buffer

--- efb.el
+++ efb.el
@@ -1,11 +1,10 @@
 ;; -*- lexical-binding: t; -*-
-
 ;;; efb.el --- An Electronic Flight Bag major mode
 ;; Copyright (C) 2025 Brian Tomlinson
 
 ;; Author: Brian Tomlinson <darthlukan at gmail dot com>
 ;; Version: 0.0.1
-;; Package-Requires: ((emacs "24.1"))
+;; Package-Requires: ((emacs "24.3"))
 ;; Keywords: efb, flight-bag, flight-planner, aviation
 ;; URL: https://github.com/darthlukan/efb-mode
 
@@ -22,8 +21,11 @@
 
 (define-derived-mode efb-mode text-mode "efb"
   "Major mode implementing a basic Electronic Flight Bag."
-  (setq-local efb-departure-icao nil)
-  (setq-local efb-arrival-icao nil))
+  (setq-local efb-departure-icao "KHMT"
+              efb-arrival-icao "KSEZ")
+
+  (insert efb-departure-icao ?\n efb-arrival-icao ?\n)
+  (buffer-string))
 
 (provide 'efb)
 ;;; efb.el ends here
