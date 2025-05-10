488a4a7c13d547b82d9e2ff26f69591675a2c5ec
Brian Tomlinson <darthlukan@gmail.com>
2025-05-10 01:19:43 -0700

    Request the METARs for arrival and destination ICAO

--- efb.el
+++ efb.el
@@ -14,6 +14,7 @@
 
 ;;; Code:
 (require 'widget)
+(require 'request)
 
 (eval-when-compile
   (require 'wid-edit))
@@ -30,15 +31,53 @@
   (widget-insert "EFB Mode: Plan your VFR Flight.\n\n")
   (efb-mode))
 
+(defvar-local efb-weather-base-url "https://aviationweather.gov/api/data/")
+(defvar-local efb-weather-type-metar "metar")
+(defvar-local efb-weather-icao-key "ids")
+
+(defun efb-get-metar (icao)
+  "Retrieve and return the METAR string for ICAO."
+  (request
+    (format "%s/%s?%s=%s" efb-weather-base-url efb-weather-type-metar efb-weather-icao-key icao)
+    :type "GET"
+    :parser 'buffer-string
+    :success (cl-function
+              (lambda (&key data &allow-other-keys)
+                (progn
+                  (message "Got: %s" data)
+                  data)))
+    :error (cl-function
+            (lambda (&rest args &key error-thrown &allow-other-keys)
+              (progn
+                (message "Error: %s" error-thrown)
+                error-thrown)))))
+
+(defun efb-get-taf (icao)
+  "Retrieve and return the TAF string for ICAO."
+  icao)
+
+(defun efb-get-notams (icao)
+  "Retrieve and return the NOTAMS for ICAO."
+  icao)
+
+(defun efb-get-tfrs (icao)
+  "Retrieve and return the TFRS for ICAO."
+  icao)
+
 (defun efb-action (depart-widget arrival-widget)
   "Display the value of DEPART-WIDGET and ARRIVAL-WIDGET."
+  (setq-local dep-icao (widget-value depart-widget))
+  (setq-local arr-icao (widget-value arrival-widget))
+  (setq-local dep-metar (efb-get-metar dep-icao))
+  (setq-local arr-metar (efb-get-metar arr-icao))
   (progn
-    (message "Departing: %s" (widget-value depart-widget))
-    (message "Arriving: %s" (widget-value arrival-widget))))
+    (message "Departing: %s, Arriving: %s" dep-icao arr-icao)
+    (message "Dep METAR: %s" dep-metar)
+    (message "Arr METAR: %s" arr-metar)))
 
 (define-derived-mode efb-mode text-mode "efb"
   "Major mode implementing a basic Electronic Flight Bag."
-  (setq-local efb-default-departure-icao "KHMT"
+  (setq-local efb-default-departure-icao "KRIV"
               efb-default-arrival-icao "KSEZ")
 
   (setq-local efb-departure-widget (widget-create 'string
@@ -46,15 +85,15 @@
                                                   :value efb-default-departure-icao
                                                   :format "Departure ICAO: %v"
                                                   :help-echo "Origin Airport"))
-  (widget-insert "\n\n")
+  (widget-insert "\s\s")
   (setq-local efb-arrival-widget (widget-create 'string
                                                 :size 12
                                                 :value efb-default-arrival-icao
                                                 :format "Arrival ICAO: %v"
                                                 :help-echo "Destination Airport"))
-  (widget-insert "\n\n\n\n")
+  (widget-insert "\n\n")
   (setq-local efb-submit-button (widget-create 'push-button
-                                               :action (lambda (&rest ignore)
+                                               :action (lambda (&rest _)
                                                          (efb-action efb-departure-widget efb-arrival-widget))
                                                "Submit"))
   (use-local-map widget-keymap)
